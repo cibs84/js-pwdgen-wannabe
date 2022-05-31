b81484ccb86e421e01ddcce57afbe33f79c58137
cibs84 <d.tilesi@gmail.com>
2022-05-31 14:51:38 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// INPUT - Nome, cognome e colore preferito dell'utente
+const userName = prompt('Inserirsci il tuo nome');
+const userSurname = prompt('Inserisci il tuo cognome');
+const favouriteColor = prompt('Inserisci il tuo colore preferito');
+
+// LOGICA - Concatenare le tre costanti in una variabile
+let userPwd = userName + userSurname + favouriteColor + 21;
+
+// OUTPUT - Scrivere la password generata nel tag h1#pwd
+document.getElementById('pwd').innerHTML = userPwd;
